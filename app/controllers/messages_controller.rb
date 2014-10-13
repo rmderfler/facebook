178d94c9d12b61b72ca5596c9506cd028345ce09
Derfler <rmderfler@gmail.com>
2014-10-12 23:39:31 -0700

    add sender_email to messages table

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -69,6 +69,6 @@ class MessagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
-      params.require(:message).permit(:name, :recipient, :message)
+      params.require(:message).permit(:name, :recipient, :message, :sender_email)
     end
 end
---
+++ db/migrate/20141013062957_add_sender_email_to_messages.rb
@@ -0,0 +1,5 @@
+class AddSenderEmailToMessages < ActiveRecord::Migration
+  def change
+    add_column :messages, :sender_email, :string
+  end
+end
