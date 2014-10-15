1212d1a3b0e03623ef06df704f7c09b381033a9f
Derfler <rmderfler@gmail.com>
2014-10-14 20:57:52 -0700

    add flash notice with helper method in application_helper.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
   has_many :messages
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
