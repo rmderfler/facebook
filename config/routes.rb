948a3a6ffb8a8c3c11123a41b2d9a3a6c853db21
Derfler <rmderfler@gmail.com>
2014-10-14 21:41:30 -0700

    add some custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do 
+    get 'register' => 'devise/registrations#new', as: :register
+    get 'login' => 'devise/sessions#new', as: :login
+    get 'logout' => 'devise/sessions#destroy', as: :logout
+  end
+
   resources :messages
+  get 'list' => 'messages#index', as: :list
   root to: 'messages#index'
  
 
