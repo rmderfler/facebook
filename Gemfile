0607ede58bde83be2a600d7b234a2abc0055123a
Derfler <rmderfler@gmail.com>
2014-10-15 00:45:22 -0700

    add gravatar service

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'puma'
 gem 'devise'
 gem 'simple_form'
+gem 'debugger'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def gravatar_url
+    stripped_email = email.strip
+    downcased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcased_email)
+
+    "http://gravatar.com/avatar/#{hash}"
+  end
 end
+
+# <%= image_tag message.user.gravatar_url %>
